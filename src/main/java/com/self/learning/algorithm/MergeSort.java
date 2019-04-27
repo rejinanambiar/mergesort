daf57f7f820b7cf7a898ddfed89774e3654d6212
Rejina Keloth Kaitheriedathil <rejinanambiar@gmail.com>
2019-04-27 20:26:28 +0200

    Added the comments

--- src/main/java/com/self/learning/algorithm/MergeSort.java
+++ src/main/java/com/self/learning/algorithm/MergeSort.java
@@ -1,7 +1,9 @@
 package com.self.learning.algorithm;
 
 import java.util.Arrays;
+
 /**
+ * Class for Merge Sort
  * 
  * @author rejina
  *
@@ -9,42 +11,45 @@ import java.util.Arrays;
 public class MergeSort {
 
 	/**
-	 * 
+	 * Main method
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[] arr = { 150, 50, 100, 200, 300, 250, 400, 350 };
-		int[] finalArray = devideAndSort(arr);
-		System.out.println("--------------------------------------------");
-		printArray(finalArray);
+		int[] inputArray = { 150, 50, 100, 200, 300, 250, 400, 350 };
+		printArray(inputArray,"Input");
+		int[] finalArray = devideAndSort(inputArray);
+		printArray(finalArray,"Output");
+		
 	}
 
 	/**
+	 * Method to divide the input array into smaller
 	 * 
-	 * @param arr
+	 * @param inputArray
 	 * @return
 	 */
-	private static int[] devideAndSort(int[] arr) {
+	private static int[] devideAndSort(int[] inputArray) {
 
-		int mid = arr.length / 2;
+		int mid = inputArray.length / 2;
 
 		if (mid <= 0) {
 			return null;
 		}
 
-		int[] leftArray = Arrays.copyOfRange(arr, 0, mid);
+		int[] leftArray = Arrays.copyOfRange(inputArray, 0, mid);
 
-		int[] rightArray = Arrays.copyOfRange(arr, mid, arr.length);
+		int[] rightArray = Arrays.copyOfRange(inputArray, mid, inputArray.length);
 
+		//Call the method iteratively to divide and merge
 		devideAndSort(rightArray);
 		devideAndSort(leftArray);
-
-		merge(arr, leftArray, rightArray);
-		printArray(arr);
-		return arr;
+		//Merge the arrays
+		merge(inputArray, leftArray, rightArray);
+		return inputArray;
 	}
 
 	/**
+	 * Method to sort and merge the arrays
 	 * 
 	 * @param arr
 	 * @param leftArray
@@ -52,11 +57,11 @@ public class MergeSort {
 	 * @return
 	 */
 	private static int[] merge(int[] arr, int[] leftArray, int[] rightArray) {
-
 		int k = 0;
 		int i = 0;
 		int j = 0;
 
+		//Compare the left and right arrays and update the main array
 		while (i < leftArray.length && j < rightArray.length) {
 			if (leftArray[i] < rightArray[j]) {
 				arr[k] = leftArray[i];
@@ -69,12 +74,15 @@ public class MergeSort {
 				k++;
 			}
 		}
+		
+		//Copy the remaining elements in the left array into the main array
 		while (i < leftArray.length) {
 			arr[k] = leftArray[i];
 			i++;
 			k++;
 		}
 
+		//Copy the remaining elements in the right array into the main array
 		while (j < rightArray.length) {
 			arr[k] = rightArray[j];
 			j++;
@@ -84,14 +92,12 @@ public class MergeSort {
 	}
 
 	/**
+	 * Method to print the array
 	 * 
 	 * @param finalArray
 	 */
-	private static void printArray(int[] finalArray) {
-		for (int i = 0; i < finalArray.length; i++) {
-			System.out.print(finalArray[i] + ",");
-		}
-		System.out.println();
+	private static void printArray(int[] array, String type) {
+		System.out.println(type+"::"+Arrays.toString(array));
 	}
 
 }
